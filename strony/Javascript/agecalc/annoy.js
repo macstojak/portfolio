a4628ba1c8004a522eef923df1660051090b1423
MacStojak <maciej.stojak89@wp.pl>
2018-06-28 14:16:26 +0200

    remove all unnecesary stuff

--- strony/Javascript/agecalc/annoy.js
+++
@@ -1,5 +0,0 @@
-var answer = prompt("Are we there yet?");
-while(answer.indexOf("yes")==-1 && answer.indexOf("yeah")==-1){
-	answer = prompt("Are we there yet?");
-}
-alert ("Yay we made it.");
--- strony/Javascript/agecalc/script.js
+++
@@ -1,6 +0,0 @@
-var d = new Date();
-var dateNow = d.getFullYear()
-var birth = prompt("Give me your year of birth");
-var age = dateNow - birth;
-var ageDays = age * 365;
-alert("Your age is "+age+ ", which is "+ageDays+" days.");
